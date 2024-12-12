9edeb5516d09235480819274a263fb17cdfe194d
minji0916 <a2927036@naver.com>
2024-12-12 09:26:30 +0900

    fix & feat: (fix) package.json update, image 업로드 e2e test에서 리소스를 정리할 때, 파일 경로를 못가져오는 문제 해결(상대경로->절대경로 사용), (feat) test할 때 db의 모든 collections을 지우는 코드 추가

--- e2e/delete_users_db.py
+++ e2e/delete_users_db.py
@@ -3,8 +3,17 @@ from pymongo import MongoClient
 # MongoDB 클라이언트 연결
 client = MongoClient("mongodb://localhost:27017/")  # 연결 URL 수정 필요
 db = client["bootcampchat"]  # 데이터베이스 이름
-users_collection = db["users"]
 
 # 컬렉션의 모든 문서 삭제
-result = users_collection.delete_many({})
-print(f"{result.deleted_count} documents deleted.")
+result = db["users"].delete_many({})
+print(f"users collections : {result.deleted_count} documents deleted.")
+result = db["files"].delete_many({})
+print(f"files collections : {result.deleted_count} documents deleted.")
+result = db["messages"].delete_many({})
+print(f"messages collections : {result.deleted_count} documents deleted.")
+result = db["rooms"].delete_many({})
+print(f"rooms collections : {result.deleted_count} documents deleted.")
+
+
+
+
